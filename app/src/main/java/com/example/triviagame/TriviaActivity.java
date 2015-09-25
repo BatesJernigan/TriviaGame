b62c27cd1343819dfc064d02b1f0853f9223a0f0
batesjernigan <bates1012@aol.com>
2015-09-25 11:00:18 -0400

    Make main activity route to appropriate activities.

--- app/src/main/java/com/example/triviagame/TriviaActivity.java
+++ app/src/main/java/com/example/triviagame/TriviaActivity.java
@@ -1,45 +1,22 @@
 package com.example.triviagame;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    final static int CREATE_CODE = 201;
-    final static int DELETE_CODE = 204;
+public class TriviaActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void changeToStartTriviaAct(View view) {
-        Intent intent = new Intent(MainActivity.this, TriviaActivity.class);
-        startActivity(intent);
-    }
-
-    public void changeToCreateQuestionAct(View view) {
-        Intent intent = new Intent(MainActivity.this, CreateQuestionActivity.class);
-        startActivityForResult(intent, CREATE_CODE);
-    }
-
-    public void changeToDeleteAllAct(View view) {
-        Log.d("demo", "clicked delete");
-        // http request for delete all
-    }
-
-    public void changeToExitAct(View view) {
-        finish();
+        setContentView(R.layout.activity_trivia);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_trivia, menu);
         return true;
     }
 
